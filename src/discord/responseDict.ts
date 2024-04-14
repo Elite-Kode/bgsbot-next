b9ab19ea4331c8d928994247ac7ffac918fca68e
Romano Lucchesi <me@romanolucchesi.com>
2024-04-14 19:30:43 +0100

    feat: add some more responses and remove unused ones

--- src/discord/responseDict.ts
+++ src/discord/responseDict.ts
@@ -16,17 +16,21 @@ export class Responses {
     'No need to hurry. Give me the details one by one'
   ]
   public static readonly NOTACOMMAND = [
-    'Um...were you trying to give me a command? If so you may be using the wrong one'
+    'Um...were you trying to give me a command? If so you may be using the wrong one',
+    "Sorry, I couldn't find that command"
   ]
-  public static readonly INSUFFICIENTPERMS = ["You don't have the permissions to make me do that"]
+  public static readonly INSUFFICIENTPERMS = [
+    "You don't have the permissions to make me do that",
+    "I'm sorry, I'm afraid I can't do that",
+    "You can't do this. Ask someone with more permissions"
+  ]
   public static readonly IDNOTFOUND = ['The ID you entered does not exist']
-  public static readonly GUILDNOTSETUP = ['Your guild has not been setup yet.']
   public static readonly NOTATEXTCHANNEL = ['The entered channel is not a text channel. Please enter a text channel']
   public static readonly EMBEDPERMISSION = [
     "I don't have permissions to send a message and/or create an embed and/or attach files in the entered channel. Please assign the permissions to me."
   ]
   public static readonly USESLASH = ['Please use the slash command instead!']
-  public static readonly ALREADYADDED = ['This has already been added']
+  public static readonly ALREADYADDED = ['This has already been added', 'Already added! Try something else']
 
   public static getResponse(action: string[]): string {
     return action[Math.floor(Math.random() * action.length)]
